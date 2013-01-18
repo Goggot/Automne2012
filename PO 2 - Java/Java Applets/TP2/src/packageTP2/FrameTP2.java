caf6584914a6c4bcb63d679c74f191ae29920196
Erwan <erwan.benard@gmail.com>
2013-01-18 10:51:08 -0500

    fhhk
    Signed-off-by: Erwan <erwan.benard@gmail.com>

---
+++ PO 2 - Java/Java Applets/TP2/src/packageTP2/FrameTP2.java
@@ -0,0 +1,28 @@
+package packageTP2;
+
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+public class FrameTP2
+  extends JFrame
+{
+  public FrameTP2()
+  {
+    try
+    {
+      jbInit();
+    }
+    catch (Exception e)
+    {
+      e.printStackTrace();
+    }
+  }
+
+  private void jbInit()
+    throws Exception
+  {
+    this.getContentPane().setLayout( null );
+    this.setSize( new Dimension(400, 300) );
+  }
+}
