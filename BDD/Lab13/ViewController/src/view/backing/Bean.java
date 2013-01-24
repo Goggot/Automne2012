e5abba06e376a9b0161cf91011daf1f9d7ccedb6
Erwan <erwan.benard@gmail.com>
2013-01-24 10:05:08 -0500

    Mises à jour auto!

---
+++ BDD/Lab13/ViewController/src/view/backing/Bean.java
@@ -0,0 +1,207 @@
+package view.backing;
+
+
+import oracle.adf.view.rich.component.rich.RichDocument;
+import oracle.adf.view.rich.component.rich.RichForm;
+import oracle.adf.view.rich.component.rich.data.RichTable;
+import oracle.adf.view.rich.component.rich.layout.RichPanelGroupLayout;
+import oracle.adf.view.rich.component.rich.layout.RichPanelStretchLayout;
+import oracle.adf.view.rich.component.rich.nav.RichCommandButton;
+import oracle.adf.view.rich.component.rich.output.RichMessages;
+import oracle.adf.view.rich.component.rich.output.RichOutputText;
+import oracle.adf.view.rich.component.rich.output.RichPanelCollection;
+
+public class Bean
+{
+  private RichPanelStretchLayout psl1;
+  private RichForm f1;
+  private RichDocument d1;
+  private RichPanelCollection pc1;
+  private RichTable t1;
+  private RichMessages m1;
+  private RichPanelGroupLayout pgl1;
+  private RichTable t2;
+  private RichPanelGroupLayout pgl2;
+  private RichOutputText ot1;
+  private RichOutputText ot2;
+  private RichPanelGroupLayout pgl3;
+  private RichOutputText ot3;
+  private RichPanelGroupLayout pgl4;
+  private RichOutputText ot4;
+  private RichCommandButton cb2;
+  private RichCommandButton cb1;
+
+  public void setPsl1(RichPanelStretchLayout psl1)
+  {
+    this.psl1 = psl1;
+  }
+
+  public RichPanelStretchLayout getPsl1()
+  {
+    return psl1;
+  }
+
+  public void setF1(RichForm f1)
+  {
+    this.f1 = f1;
+  }
+
+  public RichForm getF1()
+  {
+    return f1;
+  }
+
+  public void setD1(RichDocument d1)
+  {
+    this.d1 = d1;
+  }
+
+  public RichDocument getD1()
+  {
+    return d1;
+  }
+
+  public void setPc1(RichPanelCollection pc1)
+  {
+    this.pc1 = pc1;
+  }
+
+  public RichPanelCollection getPc1()
+  {
+    return pc1;
+  }
+
+  public void setT1(RichTable t1)
+  {
+    this.t1 = t1;
+  }
+
+  public RichTable getT1()
+  {
+    return t1;
+  }
+
+  public void setM1(RichMessages m1)
+  {
+    this.m1 = m1;
+  }
+
+  public RichMessages getM1()
+  {
+    return m1;
+  }
+
+  public void setPgl1(RichPanelGroupLayout pgl1)
+  {
+    this.pgl1 = pgl1;
+  }
+
+  public RichPanelGroupLayout getPgl1()
+  {
+    return pgl1;
+  }
+
+  public void setT2(RichTable t2)
+  {
+    this.t2 = t2;
+  }
+
+  public RichTable getT2()
+  {
+    return t2;
+  }
+
+
+  public void setPgl2(RichPanelGroupLayout pgl2)
+  {
+    this.pgl2 = pgl2;
+  }
+
+  public RichPanelGroupLayout getPgl2()
+  {
+    return pgl2;
+  }
+
+  public void setOt1(RichOutputText ot1)
+  {
+    this.ot1 = ot1;
+  }
+
+  public RichOutputText getOt1()
+  {
+    return ot1;
+  }
+
+
+  public void setOt2(RichOutputText ot2)
+  {
+    this.ot2 = ot2;
+  }
+
+  public RichOutputText getOt2()
+  {
+    return ot2;
+  }
+
+
+  public void setPgl3(RichPanelGroupLayout pgl3)
+  {
+    this.pgl3 = pgl3;
+  }
+
+  public RichPanelGroupLayout getPgl3()
+  {
+    return pgl3;
+  }
+
+  public void setOt3(RichOutputText ot3)
+  {
+    this.ot3 = ot3;
+  }
+
+  public RichOutputText getOt3()
+  {
+    return ot3;
+  }
+
+  public void setPgl4(RichPanelGroupLayout pgl4)
+  {
+    this.pgl4 = pgl4;
+  }
+
+  public RichPanelGroupLayout getPgl4()
+  {
+    return pgl4;
+  }
+
+  public void setOt4(RichOutputText ot4)
+  {
+    this.ot4 = ot4;
+  }
+
+  public RichOutputText getOt4()
+  {
+    return ot4;
+  }
+
+
+  public void setCb2(RichCommandButton cb2)
+  {
+    this.cb2 = cb2;
+  }
+
+  public RichCommandButton getCb2()
+  {
+    return cb2;
+  }
+
+  public void setCb1(RichCommandButton cb1)
+  {
+    this.cb1 = cb1;
+  }
+
+  public RichCommandButton getCb1()
+  {
+    return cb1;
+  }
+}
